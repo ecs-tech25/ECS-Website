4d07a11b971247073d7dbc2e3f2df28212b762f5
Maverick-04507 <Shashwatp673@gmail.com>
2025-03-14 14:48:28 +0530

    cors

--- Backend-ECS/src/app.js
+++ Backend-ECS/src/app.js
@@ -5,12 +5,12 @@ import cookieParser from "cookie-parser";
 
 const app=express()
 
-// app.use(
-//     cors({
-//         origin:process.env.CORS_ORIGIN,
-//         credentials:true
-//     })
-// )
+app.use(
+    cors({
+        origin:process.env.CORS_ORIGIN,
+        credentials:true
+    })
+)
 
 //Commonly Used Middlewares
 app.use(express.json({limit:"16kb"}))
